6763bc79cc584f2581da1a1481e36de1cd2509d7
Linando Saputra <linandosaputra@yahoo.co.id>
2019-07-09 19:15:53 +0700

    Initial Commit

---
+++ MC2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC2
+//
+//  Created by Linando Saputra on 09/07/19.
+//  Copyright © 2019 Linando Saputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
