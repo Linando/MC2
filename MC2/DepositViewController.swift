b25e67f418c9ded0b825d2929c6f17f005454809
Evan Christian <evanchristian98@gmail.com>
2019-07-12 12:22:27 +0700

    userDefault untuk balance, depositViewController, stockViewController

---
+++ MC2/DepositViewController.swift
@@ -0,0 +1,59 @@
+//
+//  DepositViewController.swift
+//  MC2
+//
+//  Created by Evan Christian on 12/07/19.
+//  Copyright © 2019 Linando Saputra. All rights reserved.
+//
+
+import UIKit
+
+class DepositViewController: UIViewController {
+    @IBOutlet weak var moneyTextField: UITextField!
+    @IBOutlet weak var balanceTotalLabel: UILabel!
+    @IBOutlet weak var depositButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        moneyTextField.keyboardType = UIKeyboardType.numberPad
+        depositButton.isEnabled = false
+        depositButton.alpha = 0.5
+        //User Defaults
+        let balance = UserDefaults.standard.integer(forKey: "balance")
+        balanceTotalLabel.text = "\(balance)"
+    }
+    
+    
+    
+    
+    @IBAction func moneyTextFieldEditingChanged(_ sender: Any) {
+        print("berhasil")
+        depositButton.isEnabled = true
+        depositButton.alpha = 1
+    }
+    
+    
+    
+    @IBAction func depositButtonTapped(_ sender: Any) {
+        let balance:Int = Int(balanceTotalLabel.text!)!
+        let moneyInput:Int = Int(moneyTextField.text!)!
+        balanceTotalLabel.text = "\(balance + moneyInput)"
+        UserDefaults.standard.set(Int(balanceTotalLabel.text!)!, forKey: "balance")
+        moneyTextField.text = ""
+        depositButton.isEnabled = false
+        depositButton.alpha = 0.5
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MC2/StockViewController.swift
@@ -0,0 +1,36 @@
+//
+//  StockViewController.swift
+//  MC2
+//
+//  Created by Evan Christian on 12/07/19.
+//  Copyright © 2019 Linando Saputra. All rights reserved.
+//
+
+import UIKit
+
+class StockViewController: UIViewController {
+
+    @IBOutlet weak var balanceTotalLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let balance = UserDefaults.standard.integer(forKey: "balance")
+        if balance > 0{
+            balanceTotalLabel.text = "\(balance)"
+        }else{
+            balanceTotalLabel.text = "0"
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
