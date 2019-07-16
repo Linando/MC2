296681a1261ddc16e53211e0a5898f414e656732
Evan Christian <evanchristian98@gmail.com>
2019-07-16 09:08:12 +0700

    table detail controller & iboutlet

---
+++ MC2/Controller/TableDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TableDetailViewController.swift
+//  MC2
+//
+//  Created by Evan Christian on 16/07/19.
+//  Copyright © 2019 Linando Saputra. All rights reserved.
+//
+
+import UIKit
+
+class TableDetailViewController: UIViewController {
+
+    @IBOutlet weak var stockNameLabel: UILabel!
+    @IBOutlet weak var stockPriceLabel: UILabel!
+    @IBOutlet weak var stockChangeLabel: UILabel!
+    @IBOutlet weak var totalBalanceLabel: UILabel!
+    @IBOutlet weak var stockNameSellLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
